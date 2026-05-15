bc512024c38aa93a8cb526ab3c17f2138de5533c
hafizmusanadeem <musanad04@gmail.com>
2026-05-16 02:45:11 +0500

    Fastapi Version 3 Dynamic Route

--- fastapi-ecommerce/app/main.py
+++ fastapi-ecommerce/app/main.py
@@ -17,11 +17,27 @@ def product():
 # Query
 @app.get("/items")
 def list_product(
-    name:str = Query(
+    name : str = Query(
         default=None,
-        min=1,
-        max=50,
+        min_length=1,
+        max_length=50,
         description="Search by product name(case_sensitive)"
+    ),
+    sort_by_price : bool = Query(
+        default=False,
+        description="Sort products by price"
+    ),
+    order : str = Query(
+        default="asc",
+        description="Sort Order when sort by prices(asc,desc)"
+    ),
+    limit : int = Query(
+        default = 3,
+        description="Sets a product_show limit for better readability"
+    ),
+    offset : int = Query(
+        default = 0,
+        description = "Sets a product_show pagination for start pagination offset"
     )
 ) -> dict[str, Any]:
     
@@ -36,4 +52,25 @@ def list_product(
                 status_code=404,
                 detail=f"No product found matching name={name}"
             )         
-    return {"total": len(products), "items": products}
+
+    if sort_by_price:
+        reverse = order == "desc"
+        products = (sorted(products, key=lambda p:p.get("price",0), reverse = reverse))  
+
+    total = len(products)
+    products = products[offset:(offset+limit)]  
+
+    return {"total": total, "items": products}
+
+
+@app.get("/products/{product_id}")
+def get_product_by_id(product_id:str):
+    products = get_products()
+    for product in products:
+        if product["id"] == product_id:
+            return product
+    raise HTTPException(
+        status_code=404,
+        detail= f"Product for the matching id:{product_id} not found"
+    )
+
